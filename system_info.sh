dceca0454442a66240f2ce52fca0efd8b77f76ed
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2021-03-02 01:09:22 -0500

    Create system_info.sh

---
+++ system_info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Title
+#System_Info_Script
+
+echo "This is the username=$USER"
+echo "This is the home directory=$HOME"
+echo "This is pwd info=$PWD"
+echo "This is the bash info=$BASH
+echo "This is UID info=$UID"
